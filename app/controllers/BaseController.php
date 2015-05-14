c72b83009738e58df51128303cf4179e1ace98b9
jsxqf <js-xqf@hotmail.com>
2015-05-14 22:23:30 +0800

    add sweet-alert

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -20,10 +20,14 @@ class BaseController extends \Phalcon\Mvc\Controller {
             ->addCss("$dir/css/normalize$min.css")
             ->addCss("$dir/css/skeleton$min.css")
             ->addCss("$dir/css/evil-icons$min.css")
+            ->addCss("$dir/css/sweet-alert$min.css")
+            ->addCss("$dir/css/ie9$min.css")
             ->addCss("$dir/css/index$min.css");
 
         $this->assets
-            ->addJs("$dir/js/evil-icons$min.js");
+            ->addJs("$dir/js/evil-icons$min.js")
+            ->addJs("$dir/js/jquery$min.js")
+            ->addJs("$dir/js/sweet-alert$min.js");
     }
 
 }
